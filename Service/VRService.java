9cd4f963867a3e898402393c9c7afe6b5cd2505c
garuda <garuda_xc@126.com>
2016-01-13 19:04:48 +0800

    8

---
+++ Service/VRService.java
@@ -0,0 +1,27 @@
+package android.vr;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class VRService {
+    private static final String TAG = "VRService";
+
+
+	public VRService()
+	{
+		Log.i(TAG, "VRService: construct");
+	}
+
+
+	public int foo(int a)
+	{
+		return a;
+	}
+
+}
